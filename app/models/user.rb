4647667685dbce0b5453962add71f874b8ebfddd
nhattrung <trungtranqn91@gmail.com>
2017-02-10 16:31:33 +0700

    edit file user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 	   has_many :microposts
 	   validates :name, presence: true 
-	   validates :email, presence: true 
+	   validates :email, presence: true
+	   #ssssssssssssssssssssss 
 end
